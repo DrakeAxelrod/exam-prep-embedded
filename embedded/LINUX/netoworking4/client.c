6fb50165fc600563dc005e24c9d8f690193b143a
Drake Axelrod <drake@draxel.io>
2022-03-14 14:33:27 +0100

    update

--- embedded/LINUX/netoworking4/client.c
+++ embedded/LINUX/netoworking4/client.c
@@ -1,69 +1,97 @@
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
-void func(int sockfd)
+
+#include <arpa/inet.h>
+
+#define PORT "3490" // the port client will be connecting to
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-  char buff[MAX];
-  int n;
-  for (;;)
+  if (sa->sa_family == AF_INET)
   {
-    write(sockfd, "Hello World", sizeof("Hello World"));
-    sleep(3);
-    // bzero(buff, sizeof(buff));
-    // printf("Enter the string : ");
-    // n = 0;
-    // while ((buff[n++] = getchar()) != '\n')
-    //   ;
-    // write(sockfd, buff, sizeof(buff));
-    bzero(buff, sizeof(buff));
-    read(sockfd, buff, sizeof(buff));
-    // printf("From Server : %s", buff);
-    // if ((strncmp(buff, "exit", 4)) == 0)
-    // {
-    //   printf("Client Exit...\n");
-    //   break;
-    // }
+    return &(((struct sockaddr_in *)sa)->sin_addr);
   }
+
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-int main()
+int client()
 {
-  int sockfd, connfd;
-  struct sockaddr_in servaddr, cli;
+  int sockfd, numbytes;
+  char buf[MAXDATASIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
 
-  // socket create and verification
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1)
+  // if (argc != 2)
+  // {
+  //   fprintf(stderr, "usage: client hostname\n");
+  //   exit(1);
+  // }
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo("127.0.0.1", PORT, &hints, &servinfo)) != 0)
   {
-    printf("socket creation failed...\n");
-    exit(0);
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
   }
-  else
-    printf("Socket successfully created..\n");
-  bzero(&servaddr, sizeof(servaddr));
 
-  // assign IP, PORT
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  servaddr.sin_port = htons(PORT);
-
-  // connect the client socket to server socket
-  if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+  // loop through all the results and connect to the first we can
+  for (p = servinfo; p != NULL; p = p->ai_next)
   {
-    printf("connection with the server failed...\n");
-    exit(0);
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                         p->ai_protocol)) == -1)
+    {
+      perror("client: socket");
+      continue;
+    }
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sockfd);
+      perror("client: connect");
+      continue;
+    }
+
+    break;
   }
-  else
-    printf("connected to the server..\n");
 
-  // function for chat
-  func(sockfd);
+  if (p == NULL)
+  {
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }
 
-  // close the socket
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+  printf("client: connecting to %s\n", s);
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1)
+  {
+    perror("recv");
+    exit(1);
+  }
+
+  buf[numbytes] = '\0';
+
+  printf("client: received '%s'\n", buf);
+
   close(sockfd);
+
+  return 0;
 }
--- embedded/LINUX/netoworking4/server.c
+++ embedded/LINUX/netoworking4/server.c
@@ -1,105 +1,145 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
 #include <stdio.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-// Function designed for chat between client and server.
-void func(int connfd)
+#define PORT "3490" // the port users will be connecting to
+
+#define BACKLOG 10 // how many pending connections queue will hold
+
+void sigchld_handler(int s)
 {
-  char buff[MAX];
-  int n;
-  // infinite loop for chat
-  for (;;)
-  {
-    // bzero(buff, MAX);
-    // buff = "hello world";
-    write(connfd, "Hello World", sizeof("Hello World"));
-    sleep(3);
-    // bzero(buff, MAX);
+  // waitpid() might overwrite errno, so we save and restore it:
+  int saved_errno = errno;
 
-    // // read the message from client and copy it in buffer
-    // read(connfd, buff, sizeof(buff));
-    // // print buffer which contains the client contents
-    // printf("From client: %s\t To client : ", buff);
-    // bzero(buff, MAX);
-    // n = 0;
-    // // copy server message in the buffer
-    // while ((buff[n++] = getchar()) != '\n')
-    //   ;
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    ;
 
-    // // and send that buffer to client
-    // write(connfd, buff, sizeof(buff));
-
-    // // if msg contains "Exit" then server exit and chat ended.
-    // if (strncmp("exit", buff, 4) == 0)
-    // {
-    //   printf("Server Exit...\n");
-    //   break;
-    // }
-  }
+  errno = saved_errno;
 }
 
-// Driver function
-int main()
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-  int sockfd, connfd, len;
-  struct sockaddr_in servaddr, cli;
-
-  // socket create and verification
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1)
+  if (sa->sa_family == AF_INET)
   {
-    printf("socket creation failed...\n");
-    exit(0);
+    return &(((struct sockaddr_in *)sa)->sin_addr);
   }
-  else
-    printf("Socket successfully created..\n");
-  bzero(&servaddr, sizeof(servaddr));
 
-  // assign IP, PORT
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(PORT);
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
 
-  // Binding newly created socket to given IP and verification
-  if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+int main(void)
+{
+  int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
   {
-    printf("socket bind failed...\n");
-    exit(0);
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
   }
-  else
-    printf("Socket successfully binded..\n");
 
-  // Now server is ready to listen and verification
-  if ((listen(sockfd, 5)) != 0)
+  // loop through all the results and bind to the first we can
+  for (p = servinfo; p != NULL; p = p->ai_next)
   {
-    printf("Listen failed...\n");
-    exit(0);
-  }
-  else
-    printf("Server listening..\n");
-  len = sizeof(cli);
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                         p->ai_protocol)) == -1)
+    {
+      perror("server: socket");
+      continue;
+    }
 
-  // Accept the data packet from client and verification
-  connfd = accept(sockfd, (SA *)&cli, &len);
-  if (connfd < 0)
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                   sizeof(int)) == -1)
+    {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (p == NULL)
   {
-    printf("server accept failed...\n");
-    exit(0);
+    fprintf(stderr, "server: failed to bind\n");
+    exit(1);
   }
-  else
-    printf("server accept the client...\n");
 
-  // Function for chatting between client and server
-  func(connfd);
+  if (listen(sockfd, BACKLOG) == -1)
+  {
+    perror("listen");
+    exit(1);
+  }
 
-  // After chatting close the socket
-  close(sockfd);
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1)
+  {
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+
+  while (1)
+  { // main accept() loop
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1)
+    {
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_addr.ss_family,
+              get_in_addr((struct sockaddr *)&their_addr),
+              s, sizeof s);
+    printf("server: got connection from %s\n", s);
+
+    if (!fork())
+    {                // this is the child process
+      close(sockfd); // child doesn't need the listener
+      if (send(new_fd, "Hello, world!", 13, 0) == -1)
+        perror("send");
+      close(new_fd);
+      exit(0);
+    }
+    close(new_fd); // parent doesn't need this
+  }
+
+  return 0;
 }
